193e3712d1db0f0e960240895f10d195b32adee7
robokae <alexanderhom19@gmail.com>
2021-01-16 13:46:17 -0800

    made my first PyQt5 app

---
+++ My First PyQt5 Application/myapp.py
@@ -0,0 +1,19 @@
+# importing the PyQt5 classes that we need for the application.
+from PyQt5.QtWidgets import QApplication, QWidget
+
+# Only needed for access to command line arguments
+import sys
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = QWidget()
+window.show()   # Important! Windows are hidden by default.
+
+# Start the event loop.
+app.exec_()
+
+# Your application won't reach here until you exit and the event loop has stopped.
